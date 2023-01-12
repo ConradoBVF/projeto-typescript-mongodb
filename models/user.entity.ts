e0438cfa33288aeafd472afb2657ff704248c97a
Conrado B V Ferreira <conradobvf@hotmail.com>
2023-01-12 10:46:20 -0300

    Correção user.entity

--- models/user.entity.ts
+++ models/user.entity.ts
@@ -1,9 +1,71 @@
-import { sequelizeDb } from '../db';
+import { DataTypes, Model, Optional } from 'sequelize';
+import { sequelizeDb as sequelize } from '../db';
 
 interface IUser {
     id: number;
+    name: string;
     username: string;
     email: string;
     password: string;
     role_id: number;
+    createAt: Date;
+    updateAt: Date;
 }
+
+export type UserCreationAttributes = Optional<IUser, 'id'>
+
+export class User extends Model<IUser, UserCreationAttributes> {
+    declare id: number;
+    declare name: string;
+    declare username: string;
+    declare email: string;
+    declare password: string;
+    declare role_id: number;
+    declare createAt: Date;
+    declare updateAt: Date;
+}
+
+User.init (
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true,
+          },
+          name: {
+            type: new DataTypes.STRING(40),
+            allowNull: false,
+          },
+          username: {
+            type: new DataTypes.STRING(60),
+            allowNull: false,
+          },
+          email: {
+            type: new DataTypes.STRING(100),
+            allowNull: false,
+          },
+          password: {
+            type: new DataTypes.STRING(255),
+            allowNull: false,
+          },
+          role_id: {
+            type: new DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: 'roles',
+                key: 'id'
+            }
+          },
+          createAt: {
+            type: DataTypes.DATE,
+          },
+          updateAt: {
+            type: DataTypes.DATE,
+          },
+        },
+        {
+          sequelize,
+          tableName: 'users',
+          modelName: 'user',
+        }
+)
